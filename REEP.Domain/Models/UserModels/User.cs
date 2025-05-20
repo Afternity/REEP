4561286b37bf3f593f3ac8418232d3b9afe50a32
Максимов Андрей <a89995773757@gmail.com>
2025-05-21 01:19:15 +0300

    Создал первый вариант UserModels

--- REEP.Domain/Models/UserModels/User.cs
+++ REEP.Domain/Models/UserModels/User.cs
@@ -1,6 +1,21 @@
-﻿namespace REEP.Domain.Models.UserModels
+﻿using REEP.Domain.Models.PassportModels;
+using REEP.Domain.Models.UserModels.UserTypeModels;
+
+namespace REEP.Domain.Models.UserModels
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string SecondName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? OtherContacts {  get; set; }
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+
+        public Guid UserTypeId { get; set; }
+        public UserType UserType { get; set; } = null!;
+        public IList<EquipmentPassport> EquipmentPassports { get; set; } = [];
     }
 }
---
+++ REEP.Domain/Models/UserModels/UserTypeModels/UserType.cs
@@ -0,0 +1,9 @@
+﻿namespace REEP.Domain.Models.UserModels.UserTypeModels
+{
+    public class UserType
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public IList<User> Users { get; set; } = [];
+    }
+}
