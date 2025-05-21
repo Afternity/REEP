48d5f31e87ecd83eae6f920fb1e504e000767dbc
Максимов Андрей <a89995773757@gmail.com>
2025-05-21 15:58:06 +0300

    Создал первичный вариант WarrantyModels

---
+++ REEP.Domain/Models/WarrantyModels/Warranty.cs
@@ -0,0 +1,14 @@
+﻿using REEP.Domain.Models.PassportModels;
+
+namespace REEP.Domain.Models.WarrantyModels
+{
+    public class Warranty
+    {
+        public Guid Id { get; set; }
+        public string Name {get; set; } = string.Empty;
+        public string Description {get; set; } = string.Empty;
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public IList<EquipmentPassport> EquipmentPassports { get; set; } = [];
+    }
+}
---
+++ REEP.Domain/Models/WarrantyModels/WarrantyTypeModels/WarrantyType.cs
@@ -0,0 +1,9 @@
+﻿namespace REEP.Domain.Models.WarrantyModels.WarrantyTypeModels
+{
+    public class WarrantyType
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public IList<Warranty> Warranties { get; set; } = [];
+    }
+}
