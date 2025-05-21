f26a37b2dbeca373be1e8f31631b09cffa60e862
Максимов Андрей <a89995773757@gmail.com>
2025-05-21 16:38:08 +0300

    Создал первый вариант ContractModels

---
+++ REEP.Domain/Models/ContractModels/ContractManyToManyModels/ContractAndSupplier.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REEP.Domain.Models.ContractModels.ContractManyToManyModels
+{
+    public class ContractAndSupplier
+    {
+        [Key]
+        public Guid ContractId { get; set; }
+        public Contract Contract { get; set; } = null!;
+
+        [Key]
+        public Guid SupplierId { get; set; }
+        public Supplier Supplier { get; set; } = null!;
+
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdateDate { get; set; } 
+    }
+}
---
+++ REEP.Domain/Models/ContractModels/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using REEP.Domain.Models.ContractModels.ContractManyToManyModels;
+using REEP.Domain.Models.ContractModels.ContractTypeModels;
+
+namespace REEP.Domain.Models.ContractModels
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; } = string.Empty;
+        public string? SecondName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
+        public string? OtherName { get; set; } = string.Empty;
+        public string? Number {  get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+        public string? OtherContacts { get; set; }
+
+        public Guid SupplierTypeId { get; set; }
+        public SupplierType SupplierType { get; set; } = null!;
+        public IList<ContractAndSupplier> ContractsAndSuppliers { get; set; } = [];
+    }
+}
