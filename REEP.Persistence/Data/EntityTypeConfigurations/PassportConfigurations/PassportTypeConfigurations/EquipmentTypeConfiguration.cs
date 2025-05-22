ef80bc8aa13cf2bad3320894c06f5c15d3067caa
Максимов Андрей <a89995773757@gmail.com>
2025-05-22 14:09:23 +0300

    Сделал первичный вариант PassportTypeConfigurations

--- REEP.Persistence/Data/EntityTypeConfigurations/PassportConfigurations/PassportTypeConfigurations/EquipmentTypeConfiguration.cs
+++ REEP.Persistence/Data/EntityTypeConfigurations/PassportConfigurations/PassportTypeConfigurations/EquipmentTypeConfiguration.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using REEP.Domain.Models.PassportModels.PassportTypeModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace REEP.Persistence.Data.EntityTypeConfigurations.PassportConfigurations.PassportTypeConfigurations
 {
-    internal class EquipmentTypeConfiguration
+    public class EquipmentTypeConfiguration : IEntityTypeConfiguration<EquipmentType>
     {
+        public void Configure(EntityTypeBuilder<EquipmentType> builder)
+        {
+            builder.HasKey(equipmentType => equipmentType.Id);
+
+            builder.HasIndex(equipmentType => equipmentType.CreateDate);
+            builder.HasIndex(equipmentType => equipmentType.UpdateDate);
+
+            builder.Property(equipmentType => equipmentType.Type)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(equipmentType => equipmentType.CreateDate)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+            builder.Property(equipmentType => equipmentType.UpdateDate)
+                .HasColumnType("timestamp with time zone");
+
+            builder.HasMany(equipmentType => equipmentType.Equipments)
+                .WithOne(equipment => equipment.EquipmentType)
+                .HasForeignKey(equipment => equipment.EquipmentTypeId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- REEP.Persistence/Data/EntityTypeConfigurations/PassportConfigurations/PassportTypeConfigurations/StatusTypeConfiguration.cs
+++ REEP.Persistence/Data/EntityTypeConfigurations/PassportConfigurations/PassportTypeConfigurations/StatusTypeConfiguration.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using REEP.Domain.Models.PassportModels.PassportTypeModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace REEP.Persistence.Data.EntityTypeConfigurations.PassportConfigurations.PassportTypeConfigurations
 {
-    internal class PassportTypeConfiguration
+    public class StatusTypeConfiguration : IEntityTypeConfiguration<StatusType>
     {
+        public void Configure(EntityTypeBuilder<StatusType> builder)
+        {
+            builder.HasKey(statusType => statusType.Id);
+
+            builder.HasIndex(statusType => statusType.CreateDate);
+            builder.HasIndex(statusType => statusType.UpdateDate);
+
+            builder.Property(statusType => statusType.Type)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(statusType => statusType.CreateDate)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+            builder.Property(statusType => statusType.UpdateDate)
+                .HasColumnType("timestamp with time zone");
+
+            builder.HasMany(statusType => statusType.Statuses)
+                .WithOne(status => status.StatusType)
+                .HasForeignKey(status => status.StatusTypeId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
