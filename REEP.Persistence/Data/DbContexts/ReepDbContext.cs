a8ff82bfd6153d65df743f02580d2e58f3eccace
Максимов Андрей <a89995773757@gmail.com>
2025-05-26 20:34:31 +0300

    доделал dbContext

--- REEP.Persistence/Data/DbContexts/ReepDbContext.cs
+++ REEP.Persistence/Data/DbContexts/ReepDbContext.cs
@@ -1,17 +1,25 @@
-﻿using REEP.Domain.Models.ContractModels.ContractHistoryModels;
-using REEP.Domain.Models.ContractModels.ContractManyToManyModels;
+﻿using REEP.Domain.Models.ContractModels.ContractManyToManyModels;
 using REEP.Domain.Models.ContractModels.ContractTypeModels;
 using REEP.Domain.Models.ContractModels;
-using REEP.Domain.Models.MaintenanceModels.MaintenanceHistoryModels;
 using REEP.Domain.Models.MaintenanceModels.MaintenanceTypeModels;
 using REEP.Domain.Models.MaintenanceModels;
-using REEP.Domain.Models.PassportModels.PassportHistory;
 using REEP.Domain.Models.PassportModels.PassportTypeModels;
 using REEP.Domain.Models.PassportModels;
 using REEP.Domain.Models.UserModels.UserTypeModels;
 using REEP.Domain.Models.UserModels;
 using REEP.Domain.Models.WarrantyModels.WarrantyTypeModels;
 using REEP.Domain.Models.WarrantyModels;
+using REEP.Persistence.Data.EntityTypeConfigurations.ContractConfigurations.ContractManyToManyConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.ContractConfigurations.ContractTypeConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.ContractConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.MaintenanceConfigurations.MaintenanceTypeConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.MaintenanceConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.PassportConfigurations.PassportTypeConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.PassportConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.UserConfigurations.UserTypeConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.UserConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.WarrantyConfigurations.WarrantyTypeConfigurations;
+using REEP.Persistence.Data.EntityTypeConfigurations.WarrantyConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -25,7 +33,6 @@ namespace REEP.Persistence.Data.DbContexts
             :base(options) =>
             _options = options;
 
-        public DbSet<ContractParametersHistory> ContractParametersHistories { get; set; }
         public DbSet<ContractAndPayment> ContractsAndPayments { get; set; }
         public DbSet<ContractAndSupplier> ContractsAndSuppliers { get; set; }
         public DbSet<ContractType> ContractTypes { get; set; }
@@ -34,11 +41,9 @@ namespace REEP.Persistence.Data.DbContexts
         public DbSet<Contract> Contracts { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
-        public DbSet<MaintenаnceParametersHistory> MaintenаnceParametersHistories { get; set; }
         public DbSet<MaintenanceType> MaintenanceTypes { get; set; }
         public DbSet<Maintenance> Maintenances { get; set; }
         public DbSet<MaintenanceRequest> MaintenanceRequests { get; set; }
-        public DbSet<EquipmentParametersHistory> EquipmentParametersHistories { get; set; }
         public DbSet<EquipmentType> EquipmentTypes { get; set; }
         public DbSet<StatusType> StatusTypes { get; set; }
         public DbSet<Equipment> Equipment {  get; set; }
@@ -53,7 +58,32 @@ namespace REEP.Persistence.Data.DbContexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new ContractAndPaymentConfiguration());
+            modelBuilder.ApplyConfiguration(new ContractAndSupplierConfiguration());
+            modelBuilder.ApplyConfiguration(new ContractTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new SupplierTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ContractConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+            modelBuilder.ApplyConfiguration(new SupplierConfiguration());
 
+            modelBuilder.ApplyConfiguration(new MaintenanceTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new MaintenanceConfiguration());
+            modelBuilder.ApplyConfiguration(new MaintenanceRequestConfiguration());
+
+            modelBuilder.ApplyConfiguration(new EquipmentTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new StatusTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EquipmentConfiguration());
+            modelBuilder.ApplyConfiguration(new EquipmentPassportConfiguration());
+            modelBuilder.ApplyConfiguration(new LocationConfiguration());
+            modelBuilder.ApplyConfiguration(new StatusConfiguration());
+            modelBuilder.ApplyConfiguration(new TechnicalParameterConfiguration());
+
+            modelBuilder.ApplyConfiguration(new UserTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            modelBuilder.ApplyConfiguration(new WarrantyTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new WarrantyConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
