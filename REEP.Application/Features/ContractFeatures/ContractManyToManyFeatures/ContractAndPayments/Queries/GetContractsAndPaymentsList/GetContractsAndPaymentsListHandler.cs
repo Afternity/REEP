a2aee96d5645ce3770d69b0be46e9143581458a0
Максимов Андрей <a89995773757@gmail.com>
2025-06-10 10:31:55 +0300

    создал рабочее пространство для Features ContractsAndPayments

---
+++ REEP.Application/Features/ContractFeatures/ContractManyToManyFeatures/ContractAndPayments/Queries/GetContractsAndPaymentsList/GetContractsAndPaymentsListHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REEP.Application.Features.ContractFeatures.ContractManyToManyFeatures.ContractAndPayments.Queries.GetContractsAndPaymentsList
+{
+    internal class GetContractsAndPaymentsListHandler
+    {
+    }
+}
---
+++ REEP.WebApi/Controllers/WarrantyControllers/WarrantyTypeControllers/WarrantyTypeController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Commands.CreateSupplierType;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Commands.HardDeleteSupplierType;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Commands.SoftDeleteSupplierType;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Commands.UpdateSupplierType;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Queries.GetSupplierTypeByTypeDetails;
+using REEP.Application.Features.ContractFeatures.ContractTypesFeatures.SupplierTypes.Queries.GetSupplierTypeDetails;
+using REEP.WebApi.Controllers.ContractControllers.ContractTypeControllers;
+
+namespace REEP.WebApi.Controllers.WarrantyControllers.WarrantyTypeControllers
+{
+    public class WarrantyTypeController
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger<WarrantyTypeController> _logger;
+
+        public WarrantyTypeController(
+            IMapper mapper,
+            ILogger<WarrantyTypeController> logger) =>
+            (_mapper, _logger) = (mapper, logger);
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateWarrantyTypeDto createSupplierTypeDto)
+        {
+            var command = _mapper.Map<CreateWarrantyTypeCommand>(createSupplierTypeDto);
+            var supplieTypeId = await Mediator.Send(command);
+            return Ok(supplieTypeId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateSupplierTypeDto updateSupplierTypeDto)
+        {
+            var command = _mapper.Map<UpdateSupplierTypeCommand>(updateSupplierTypeDto);
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut("soft-delete")]
+        public async Task<IActionResult> SoftDelete([FromBody] SoftDeleteSupplierTypeDto softDeleteSupplierTypeDto)
+        {
+            var command = _mapper.Map<SoftDeleteSupplierTypeCommand>(softDeleteSupplierTypeDto);
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> HardDelete(Guid id)
+        {
+            var command = new HardDeleteSupplierTypeCommand()
+            {
+                Id = id
+            };
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpGet("{type}/by-type")]
+        public async Task<ActionResult<SupplierTypeByTypeDetailsVm>> GetByType(string type)
+        {
+            var query = new GetSupplierTypeByTypeDetailsQuery()
+            {
+                Type = type
+            };
+            var supplierTypeByTypeDetailsVm = await Mediator.Send(query);
+            return Ok(supplierTypeByTypeDetailsVm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SupplierTypeDetailsVm>> Get(Guid id)
+        {
+            var query = new GetSupplierTypeDetailsQuery()
+            {
+                Id = id
+            };
+            var supplierTypeDetailsVm = await Mediator.Send(query);
+            return Ok(supplierTypeDetailsVm);
+        }
+
+        [HttpGet("{isDeleted}/get-all")]
+        public async Task<ActionResult<WarrantyTypeListVm>> GetAll(bool isDeleted)
+        {
+
+            var query = new GetWarrantyTypeListQuery()
+            {
+                IsDeleted = isDeleted
+            };
+            var supplierTypeDetailsVm = await Mediator.Send(query);
+            return Ok(supplierTypeDetailsVm);
+        }
+    }
+}
