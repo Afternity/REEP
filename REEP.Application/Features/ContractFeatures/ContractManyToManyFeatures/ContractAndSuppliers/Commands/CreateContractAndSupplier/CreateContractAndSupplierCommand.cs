c2e0ed7d232c46064d4f9155331d59757243b9ad
Максимов Андрей <a89995773757@gmail.com>
2025-06-10 14:27:09 +0300

    настроил рабочее пространство для Features ContractsAndSuppliers

---
+++ REEP.Application/Features/ContractFeatures/ContractManyToManyFeatures/ContractAndSuppliers/Commands/CreateContractAndSupplier/CreateContractAndSupplierCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REEP.Application.Features.ContractFeatures.ContractManyToManyFeatures.ContractAndSuppliers.Commands.CreateContractAndSupplier
+{
+    internal class CreateContractAndSupplierCommand
+    {
+    }
+}
